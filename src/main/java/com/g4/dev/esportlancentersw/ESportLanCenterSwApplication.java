7fbd15accfc146a03ae231855e5345cf7dd6b63f
Luis Developer <luismq7890@gmail.com>
2023-02-20 23:14:56 -0500

    Inicia el repositorio con la creacion del proyecto y sus dependencias

---
+++ src/main/java/com/g4/dev/esportlancentersw/ESportLanCenterSwApplication.java
@@ -0,0 +1,13 @@
+package com.g4.dev.esportlancentersw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESportLanCenterSwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESportLanCenterSwApplication.class, args);
+    }
+
+}
